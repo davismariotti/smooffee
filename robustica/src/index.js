1c195889de5ec25be6d171bb6df03ae7c5e96fcd
tomkdale <tom.k.dale@gmail.com>
2019-02-26 11:43:08 -0800

    Add robustica

---
+++ robustica/src/index.js
@@ -0,0 +1,6 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import "bootstrap/dist/css/bootstrap.css";
+import Orders from "./components/orders";
+
+ReactDOM.render(<Orders />, document.getElementById("root"));
