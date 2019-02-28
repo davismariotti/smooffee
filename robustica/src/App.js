24dcd9cc7f18deddfe776b2acd8e9ee5369164cc
tomkdale <tom.k.dale@gmail.com>
2019-02-28 12:35:07 -0800

    Login hooks to firebase now

--- robustica/src/App.js
+++ robustica/src/App.js
@@ -1,36 +1,33 @@
-import React, { Component } from 'react';
-import firebaseApp from './firebase/Firebase';
-import { hashHistory } from 'react-router'
-import Navbar from './Navbar';
-import './css/font-awesome.css'
-import './css/bootstrap-social.css';
+import React, { Component } from "react";
+import firebaseApp from "./firebase/Firebase";
+import { hashHistory } from "react-router";
+import Navbar from "./Navbar";
+import "./css/font-awesome.css";
+import "./css/bootstrap-social.css";
 
 class App extends Component {
   constructor(props) {
-      super(props);
-      this.state = {loggedin: false};
+    super(props);
+    this.state = { loggedin: false };
   }
-  componentWillMount(){
+  componentWillMount() {
     let _this = this;
     firebaseApp.auth().onAuthStateChanged(function(user) {
       if (user) {
         //if logged in...
-        _this.setState({loggedin: true});
-          hashHistory.push('/dashboard'); //after login, redirect to dashboard
+        _this.setState({ loggedin: true });
+        hashHistory.push("/dashboard"); //after login, redirect to dashboard
       } else {
         //if not logged in...
-        _this.setState({loggedin: false});
+        _this.setState({ loggedin: false });
       }
     });
-  } 
+  }
   render() {
     return (
       <div className="App">
         <div className="App-header">
-          <h4>React / Firebase with Authentication</h4>
-          <a className="btn btn-block btn-social btn-github" href="https://github.com/zerostatic/react-firebase-authentication">
-            <span className="fa fa-github"></span> View on GitHub
-          </a>
+          <h4>Smooffee</h4>
         </div>
         <Navbar loggedin={this.state.loggedin} />
         {this.props.children}
