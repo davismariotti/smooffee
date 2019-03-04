cbc068cb8e876a934f123e02c37b456c0c2b069c
Davis Mariotti <davismariotti@gmail.com>
2019-03-03 16:15:08 -0800

    Test products

--- arabica/app/actions/ProductActions.java
+++ arabica/app/actions/ProductActions.java
@@ -1,23 +1,24 @@
 package actions;
 
+import models.Organization;
 import models.Product;
 
 public class ProductActions {
-    public static Product createProduct(String name, String description, Integer price) {
+    public static Product createProduct(Long organizationId, String name, String description, Integer price) {
+        Organization organization = Organization.find.byId(organizationId);
+        if (organization == null) throw new RuntimeException("Organization not found");
         Product product = new Product();
         product.setName(name);
         product.setDescription(description);
         product.setPrice(price);
+        product.setOrganization(organization);
         product.save();
-        product.refresh();
         return product;
     }
 
     public static Product updateProduct(Long productId, String name, String description, Integer price) {
         Product product = Product.find.byId(productId);
-        if (product == null) {
-            return null;
-        }
+        if (product == null) throw new RuntimeException("Product not found");
         product.setName(name);
         product.setDescription(description);
         product.setPrice(price);
