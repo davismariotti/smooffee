84720d56a0c0c51b3afd1550b218e9a5f850f67b
Davis Mariotti <davismariotti@gmail.com>
2019-03-11 15:38:33 -0700

    Add teardown to HomeControllerTest

--- arabica/test/controllers/HomeControllerTest.java
+++ arabica/test/controllers/HomeControllerTest.java
@@ -1,23 +1,20 @@
 package controllers;
 
 import environment.FakeApplication;
-import org.junit.Before;
+import org.junit.AfterClass;
+import org.junit.BeforeClass;
 import org.junit.Test;
-import play.Application;
-import play.inject.guice.GuiceApplicationBuilder;
 import play.mvc.Http;
 import play.mvc.Result;
-import play.test.WithApplication;
 
 import static org.junit.Assert.assertEquals;
 import static play.mvc.Http.Status.OK;
 import static play.test.Helpers.GET;
-import static play.test.Helpers.route;
 
 public class HomeControllerTest {
 
-    @Before
-    public void setup() {
+    @BeforeClass
+    public static void setup() {
         FakeApplication.start(true);
     }
 
@@ -29,6 +26,11 @@ public class HomeControllerTest {
 
         Result result = FakeApplication.routeRequest(request);
         assertEquals(OK, result.status());
+    }
+
+    @AfterClass
+    public static void teardown() {
+        FakeApplication.stop();
     }
 
 }
