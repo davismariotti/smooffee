a7f787dec2e905c1c55e89404474f2e2aa350de8
Davis Mariotti <davismariotti@gmail.com>
2019-03-14 15:11:29 -0700

    Fix database issue

--- robustica/src/components/options.js
+++ robustica/src/components/options.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react'
 import MenuItem from '@material-ui/core/MenuItem'
 
-//Options create Menu items and links to pages for users depending on their authorizations
+// Options create Menu items and links to pages for users depending on their authorizations
 
 class Options extends Component {
   render() {
@@ -9,7 +9,9 @@ class Options extends Component {
       <div>
         <MenuItem>Add Cash To Accounts</MenuItem>
         <MenuItem>Edit Menu Items</MenuItem>
-        <MenuItem>y</MenuItem>
+        <MenuItem>Manage Employees (Admin)</MenuItem>
+        <MenuItem>Transaction History (Admin)</MenuItem>
+        <MenuItem>Store Analytics (Admin)</MenuItem>
       </div>
     )
   }
