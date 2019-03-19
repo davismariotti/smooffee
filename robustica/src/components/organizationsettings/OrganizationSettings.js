f80f683a7ba2b5bf0f3670c038b4b6f94dbe1206
Davis Mariotti <davismariotti@gmail.com>
2019-03-19 15:46:37 -0700

    Fix buttons on settings page

--- robustica/src/components/organizationsettings/OrganizationSettings.js
+++ robustica/src/components/organizationsettings/OrganizationSettings.js
@@ -15,11 +15,10 @@ import MenuItem from '@material-ui/core/MenuItem'
 import Button from '@material-ui/core/Button'
 import {ORGANIZATION_ID} from '../../constants'
 
-
 const styles = {
   organizationSettings: {
     textAlign: 'center'
-  }, 
+  },
   paper: {
     marginLeft: '60px',
     marginRight: '60px',
@@ -39,111 +38,90 @@ const ListProductsQuery = gql`
     }
   }
 `
-const options = [
-  'edit',
-  'add',
-];
-
-
-const ITEM_HEIGHT = 48;
 
 class OrganizationSettings extends Component {
   constructor(props) {
     super(props)
 
     this.state = {
-      optionMenu: null,
-      
+      open: [],
     }
+
+    this.anchorEls = []
 
     this.classes = props
   }
 
-  handleOptionMenuClick = event => {
-    this.setState({ optionMenu: event.currentTarget })
+  handleOptionMenuClick = id => {
+    const {open} = this.state
+    open[id] = true
+    this.setState({
+      open
+    })
   }
 
-  handleOptionClose = () => {
-    this.setState({ optionMenu: null })
+  handleOptionClose = id => {
+    const {open} = this.state
+    open[id] = false
+    this.setState({
+      open
+    })
   }
 
   render() {
-    const {classes} = this.props;
-    const {optionMenu} = this.state;
-    const open = Boolean(optionMenu);
+    const {classes} = this.props
+    const {open} = this.state
     return (
       <div>
         <Paper className={classes.paper} elevation={1}>
-        <Typography variant="h5" component="h3">
-          Products
-        </Typography>
-        <Query query={ListProductsQuery} variables={{organizationId: localStorage.getItem(ORGANIZATION_ID)}}>
-          {({loading, error, data}) => {
-            if (loading) return 'Loading'
-            if (error) return 'Error'
-            return (
-              <div>
-                <Table className={classes.table}>
-                  <TableHead>
-                    <TableRow>
-                      <TableCell align="left">Name</TableCell>
-                      <TableCell align="right">Price</TableCell>
-                      <TableCell align="right">Description</TableCell>
-                      <TableCell align="right">Options</TableCell>
-                    </TableRow>
-                  </TableHead>
-                  <TableBody>
-                    {data.product.list.map(productItem => {
-                      let buttonRef
-
-                      return (
-                        <TableRow key={productItem.id}>
-                          <TableCell align="left">
-                            {productItem.name}
-                          </TableCell>
-                          <TableCell align="right">
-                            {`$${productItem.price / 100}`}
-                          </TableCell>
-                          <TableCell align="right">
-                            {productItem.description}
-                          </TableCell>
-                          <TableCell align="right">
-                          <Button 
-                            aria-owns={optionMenu ? 'menu' : undefined}
-                            aria-haspopup="true"
-                            onClick={this.handleOptionMenuClick}
-                            buttonRef={node=> {buttonRef = node}}>
-                            <MoreVert/> 
-                          </Button>
-                            <Menu
-                               id="menu"
-                               anchorEl={buttonRef}
-                               optionMenu={optionMenu}
-                               open={open}
-                               onClose={this.handleOptionClose} 
-                               PaperProps={{
-                                style: {
-                                  maxHeight: ITEM_HEIGHT * 2.5,
-                                  width: 70,
-                                },
-                              }}>
-                              {options.map(option => (
-                              <MenuItem key={option} selected={option === 'Pyxix'} onClick={this.handleOptionClose}>
-                                {option}
-                              </MenuItem>
-                              ))}
-                            </Menu>
-
-                          </TableCell>
-                        </TableRow>
-                      )
-                    })}
-                  </TableBody>
-                </Table>
-              </div>
-            )
-          }}
-        </Query>
+          <Typography variant="h5" component="h3">
+            Products
+          </Typography>
+          <Query query={ListProductsQuery} variables={{organizationId: localStorage.getItem(ORGANIZATION_ID)}}>
+            {({loading, error, data}) => {
+              if (loading) return 'Loading'
+              if (error) return 'Error'
+              return (
+                <div>
+                  <Table className={classes.table}>
+                    <TableHead>
+                      <TableRow>
+                        <TableCell align="left">Name</TableCell>
+                        <TableCell align="right">Price</TableCell>
+                        <TableCell align="right">Description</TableCell>
+                        <TableCell align="right">Options</TableCell>
+                      </TableRow>
+                    </TableHead>
+                    <TableBody>
+                      {data.product.list.map(productItem => {
+                        return (
+                          <TableRow key={productItem.id}>
+                            <TableCell align="left">
+                              {productItem.name}
+                            </TableCell>
+                            <TableCell align="right">
+                              {`$${productItem.price / 100}`}
+                            </TableCell>
+                            <TableCell align="right">
+                              {productItem.description}
+                            </TableCell>
+                            <TableCell align="right">
+                              <Button onClick={() => {this.handleOptionMenuClick(productItem.id)}} buttonRef={node => {this.anchorEls[productItem.id] = node}}>
+                                <MoreVert/>
+                              </Button>
+                              <Menu id="menu" open={open[productItem.id] || false} anchorEl={this.anchorEls[productItem.id]} onClose={() => {this.handleOptionClose(productItem.id)}}>
+                                <MenuItem>Edit</MenuItem>
+                              </Menu>
+                            </TableCell>
+                          </TableRow>
+                        )
+                      })}
+                    </TableBody>
+                  </Table>
+                </div>
+              )
+            }}
+          </Query>
         </Paper>
       </div>
     )
