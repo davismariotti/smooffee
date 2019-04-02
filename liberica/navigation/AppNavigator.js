0176c366205041bfcc923a4f6cb3647979dd317c
tomkdale <tom.k.dale@gmail.com>
2019-04-02 10:23:55 -0700

    Get basic liberica pages working (#78)
    Improve Home Screen view
    
    Update gitignore to ignore metro-cache files
    
    Edit .gitignore changes to only affect liberica files
    
    remove all metro-cache files
    
    Create relevant settings page
    
    Refactor gitignores
    
    Add metro cache back to .gitignore
    
    Add internal navigation to new order process
    
    Move all navigation components into AppNavigation.js as it should be
    
    Add drink size picker

--- liberica/navigation/AppNavigator.js
+++ liberica/navigation/AppNavigator.js
@@ -1,10 +1,78 @@
-import React from 'react';
-import { createAppContainer, createSwitchNavigator } from 'react-navigation';
+import React from "react";
+import { Platform } from "react-native";
+import {
+  createAppContainer,
+  createSwitchNavigator,
+  createStackNavigator,
+  createBottomTabNavigator
+} from "react-navigation";
+import TabBarIcon from "../components/TabBarIcon";
+import HomeScreen from "../screens/HomeScreen";
+import SettingsScreen from "../screens/SettingsScreen";
+import { Order, Options, Information } from "../screens/NewOrderScreen";
+import LoginScreen from "../screens/LoginScreen";
+import SplashScreen from "../screens/SplashScreen";
 
-import MainTabNavigator from './MainTabNavigator';
+const AuthStack = createStackNavigator({
+  Login: LoginScreen
+});
+const HomeStack = createStackNavigator({
+  Home: HomeScreen
+});
+HomeStack.navigationOptions = {
+  tabBarLabel: "Home",
+  tabBarIcon: ({ focused }) => (
+    <TabBarIcon
+      focused={focused}
+      name={
+        Platform.OS === "ios"
+          ? `ios-information-circle${focused ? "" : "-outline"}`
+          : "md-information-circle"
+      }
+    />
+  )
+};
 
-export default createAppContainer(createSwitchNavigator({
-  // You could add another route here for authentication.
-  // Read more at https://reactnavigation.org/docs/en/auth-flow.html
-  Main: MainTabNavigator,
-}));
+const NewOrderStack = createStackNavigator({
+  Order,
+  Options,
+  Information
+});
+NewOrderStack.navigationOptions = {
+  tabBarLabel: "New Order",
+  tabBarIcon: ({ focused }) => (
+    <TabBarIcon
+      focused={focused}
+      name={Platform.OS === "ios" ? "ios-cafe" : "md-cafe"}
+    />
+  )
+};
+
+const SettingsStack = createStackNavigator({
+  Settings: SettingsScreen
+});
+SettingsStack.navigationOptions = {
+  tabBarLabel: "Settings",
+  tabBarIcon: ({ focused }) => (
+    <TabBarIcon
+      focused={focused}
+      name={Platform.OS === "ios" ? "ios-options" : "md-options"}
+    />
+  )
+};
+const TabNavigator = createBottomTabNavigator({
+  Home: HomeStack,
+  NewOrderStack,
+  SettingsStack
+});
+export default createAppContainer(
+  createSwitchNavigator(
+    {
+      App: TabNavigator,
+      Auth: AuthStack
+    },
+    {
+      initialRouteName: "App"
+    }
+  )
+);
