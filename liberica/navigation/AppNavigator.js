b8a0c8f90061d491b0299ff027e0c04619f85705
Tom Dale <tom.k.dale@gmail.com>
2019-04-09 09:37:21 -0700

    Move all navigation components into AppNavigation.js as it should be

--- liberica/navigation/AppNavigator.js
+++ liberica/navigation/AppNavigator.js
@@ -1,22 +1,78 @@
-import React from 'react'
-import { createAppContainer, createSwitchNavigator, createStackNavigator } from 'react-navigation'
-import MainTabNavigator from './MainTabNavigator'
-import HomeScreen from '../screens/HomeScreen'
-import SettingsScreen from '../screens/SettingsScreen'
-import OrderStack from '../screens/NewOrderScreen'
-import LoginScreen from '../screens/LoginScreen'
-import SplashScreen from '../screens/SplashScreen'
+import React from "react";
+import { Platform } from "react-native";
+import {
+  createAppContainer,
+  createSwitchNavigator,
+  createStackNavigator,
+  createBottomTabNavigator
+} from "react-navigation";
+import TabBarIcon from "../components/TabBarIcon";
+import HomeScreen from "../screens/HomeScreen";
+import SettingsScreen from "../screens/SettingsScreen";
+import { Order, Options, Information } from "../screens/NewOrderScreen";
+import LoginScreen from "../screens/LoginScreen";
+import SplashScreen from "../screens/SplashScreen";
 
-const AuthStack =createStackNavigator({Login: LoginScreen})
+const AuthStack = createStackNavigator({
+  Login: LoginScreen
+});
+const HomeStack = createStackNavigator({
+  Home: HomeScreen
+});
+HomeStack.navigationOptions = {
+  tabBarLabel: "Home",
+  tabBarIcon: ({ focused }) => (
+    <TabBarIcon
+      focused={focused}
+      name={
+        Platform.OS === "ios"
+          ? `ios-information-circle${focused ? "" : "-outline"}`
+          : "md-information-circle"
+      }
+    />
+  )
+};
 
-export default createAppContainer(createSwitchNavigator(
-  {
-    Loading: SplashScreen,
-    App: MainTabNavigator,
-    Order: OrderStack,
-    Auth: AuthStack,
-  },
-  {
-    initialRouteName:'App',
-  }
-));
+const NewOrderStack = createStackNavigator({
+  Order,
+  Options,
+  Information
+});
+NewOrderStack.navigationOptions = {
+  tabBarLabel: "New Order",
+  tabBarIcon: ({ focused }) => (
+    <TabBarIcon
+      focused={focused}
+      name={Platform.OS === "ios" ? "ios-cafe" : "md-cafe"}
+    />
+  )
+};
+
+const SettingsStack = createStackNavigator({
+  Settings: SettingsScreen
+});
+SettingsStack.navigationOptions = {
+  tabBarLabel: "Settings",
+  tabBarIcon: ({ focused }) => (
+    <TabBarIcon
+      focused={focused}
+      name={Platform.OS === "ios" ? "ios-options" : "md-options"}
+    />
+  )
+};
+const TabNavigator = createBottomTabNavigator({
+  Home: HomeStack,
+  NewOrderStack,
+  SettingsStack
+});
+export default createAppContainer(
+  createSwitchNavigator(
+    {
+      App: TabNavigator,
+      Auth: AuthStack
+    },
+    {
+      initialRouteName: "App"
+    }
+  )
+);
--- liberica/screens/NewOrderScreen.js
+++ liberica/screens/NewOrderScreen.js
@@ -1,141 +1,121 @@
-import React from 'react';
-import { ScrollView,
-   StyleSheet,
-   Text,
-   Button,
-   CheckBox,
-  View } from 'react-native';
+import React from "react";
+import {
+  ScrollView,
+  StyleSheet,
+  Text,
+  Button,
+  CheckBox,
+  View
+} from "react-native";
 
-import {createStackNavigator,createAppContainer} from 'react-navigation'
-
-
-
-
-
-class Order extends React.Component{
+export class Order extends React.Component {
   static navigationOptions = {
-    title: 'Order'
+    title: "Order"
   };
   render() {
-    return(
+    return (
       <ScrollView style={styles.container}>
         <Button
-         title="Smoothee"
-         onPress={() => {
-           this.props.navigation.navigate('Options', {itemId:1,item:'Smoothee',
-          });
-         }}/>
+          title="Smoothee"
+          onPress={() => {
+            this.props.navigation.navigate("Options", {
+              itemId: 1,
+              item: "Smoothee"
+            });
+          }}
+        />
         <Button
-         title="Drip Coffee"
-         onPress={() => {
-           this.props.navigation.navigate('Options', {itemId:2,item:'Drip coffee',
-          });
-         }}/>
+          title="Drip Coffee"
+          onPress={() => {
+            this.props.navigation.navigate("Options", {
+              itemId: 2,
+              item: "Drip coffee"
+            });
+          }}
+        />
       </ScrollView>
-    )
+    );
   }
 }
 
-class Options extends React.Component{
+export class Options extends React.Component {
   static navigationOptions = {
-    title: 'Options'
+    title: "Options"
   };
   render() {
-    const{navigation} = this.props
-    const itemId = navigation.getParam('itemId', 'NO-ID')
-    const drinkOrdered = navigation.getParam('item','error')
-    return(
+    const { navigation } = this.props;
+    const itemId = navigation.getParam("itemId", "NO-ID");
+    const drinkOrdered = navigation.getParam("item", "error");
+    return (
       <View style={styles.container}>
         <Text>{drinkOrdered}</Text>
         <View style={styles.sectionContainer}>
           <Text>Choose Size</Text>
-          <CheckBox
-            title='Small'
-            size='medium'
-            iconRight
-            />
-              <CheckBox
-            title='Medium'
-            />
-              <CheckBox
-            title='Large'
-            />
+          <CheckBox title="Small" size="medium" iconRight />
+          <CheckBox title="Medium" />
+          <CheckBox title="Large" />
         </View>
         <View style={styles.sectionContainer}>
           <Text>Choose Flavor</Text>
-          <CheckBox
-            title='Small'
-            size='medium'
-            iconRight
-            />
-              <CheckBox
-            title='Medium'
-            />
-              <CheckBox
-            title='Large'
-            />
+          <CheckBox title="Small" size="medium" iconRight />
+          <CheckBox title="Medium" />
+          <CheckBox title="Large" />
         </View>
         <Button
-         title="Next"
-         onPress={() => {
-           this.props.navigation.navigate('Information', {itemId:1,item:'drip coffee',
-          });
-         }}/>
+          title="Next"
+          onPress={() => {
+            this.props.navigation.navigate("Information", {
+              itemId: 1,
+              item: "drip coffee"
+            });
+          }}
+        />
       </View>
-    )
+    );
   }
 }
 
-class Information extends React.Component{
+export class Information extends React.Component {
   static navigationOptions = {
-    title: 'Information'
-  };
-  render() {
-    return(
-      <ScrollView style={styles.container}>
-        <Text>Finalize Order with notes, location, and delivery time</Text>
-        <Button
-         title="Submit"
-         onPress={() => {
-           this.props.navigation.navigate('Order', {itemId:1,item:'drip coffee',
-          });
-         }}/>
-      </ScrollView>
-    )
-  }
-}
-
-
-const OrderStack = createStackNavigator(
-  {
-    Order: Order,
-    Options: Options,
-    Information: Information
-  }
-)
-
-const OrderContainer = createAppContainer(OrderStack)
-
-export default class NewOrderScreen extends React.Component {
-  static navigationOptions = { //dont show a header
-    header: null,
+    title: "Information"
   };
   render() {
     return (
-      <OrderContainer style={styles.container}/>
+      <ScrollView style={styles.container}>
+        <Text>Finalize Order with notes, location, and delivery time</Text>
+        <Button
+          title="Submit"
+          onPress={() => {
+            this.props.navigation.navigate("Order", {
+              itemId: 1,
+              item: "drip coffee"
+            });
+          }}
+        />
+      </ScrollView>
     );
+  }
+}
+
+export default class NewOrderScreen extends React.Component {
+  static navigationOptions = {
+    //dont show a header
+    header: null
+  };
+  render() {
+    return <OrderContainer style={styles.container} />;
   }
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: "#fff"
   },
   sectionContainer: {
     fontSize: 20,
-    color:'green',
-    textAlign:'left',
-    lineHeight:30,
+    color: "green",
+    textAlign: "left",
+    lineHeight: 30
   }
 });
