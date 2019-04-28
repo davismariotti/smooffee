28f8caf79f35310d4d12ed8ef8ab29718083da88
Tom Dale <tom.k.dale@gmail.com>
2019-04-28 14:46:34 -0700

    Add new user and forgot password forms and firebase hookup

--- liberica/src/screens/auth/ForgotPasswordScreen.js
+++ liberica/src/screens/auth/ForgotPasswordScreen.js
@@ -1,44 +1,58 @@
-import React from 'react'
-import { Text, View, StyleSheet, Image, Button } from 'react-native'
-import LoginForm from './components/LoginForm'
-export default class ForgotPasswordScreen extends React.Component {
-    static navigationOptions = {
-        title: 'Login',
-        header: null,
-    }
+import React from "react";
+import { Text, View, StyleSheet, Image, Button } from "react-native";
+import PasswordRecoveryRF from "./components/PasswordRecoveryRF";
+import AuthMiddleware from "./middleware/AuthMiddleware";
+import { connect } from "react-redux";
 
-    render() {
-        return (
-            <View>
+export class ForgotPasswordScreen extends React.Component {
+  static navigationOptions = {
+    title: "ForgotPassword",
+    header: null
+  };
 
-                <Text style={styles.title}> Forgot Password</Text>
-                <Text>TODO add password recovery stuff</Text>
+  render() {
+    const { recoverWithEmail } = this.props;
 
-                <Button title="Go Back"
-                    onPress={() => {
-                        this.props.navigation.navigate('LoginScreen')
-                    }} />
-            </View>
-        )
-    }
-
-
+    const submit = ({ email }) => {
+      recoverWithEmail(email);
+    };
+    return (
+      <View>
+        <Text style={styles.title}>Send Password Recovery to Email</Text>
+        <PasswordRecoveryRF onSubmit={submit} />
+        <Button
+          title="Back to Login"
+          onPress={() => {
+            this.props.navigation.navigate("LoginScreen");
+          }}
+        />
+      </View>
+    );
+  }
+}
+const mapDispatchToProps = dispatch => {
+  return {
+    recoverWithEmail: email => AuthMiddleware.recoverWithEmail(email)(dispatch)
+  };
 };
 
+export default connect(
+  null,
+  mapDispatchToProps
+)(ForgotPasswordScreen);
 const styles = StyleSheet.create({
-    image: {
-        width: 200,
-        height: 200,
-        resizeMode: 'contain',
-        marginTop: 30,
-        marginLeft: 110
-    },
-    title: {
-        fontSize: 30,
-        marginTop: 10,
-        color: 'rgba(96,100,109, 1)',
-        lineHeight: 50,
-        textAlign: 'center'
-    }
-})
-
+  image: {
+    width: 200,
+    height: 200,
+    resizeMode: "contain",
+    marginTop: 30,
+    marginLeft: 110
+  },
+  title: {
+    fontSize: 30,
+    marginTop: 10,
+    color: "rgba(96,100,109, 1)",
+    lineHeight: 50,
+    textAlign: "center"
+  }
+});
--- liberica/src/screens/auth/NewUserScreen.js
+++ liberica/src/screens/auth/NewUserScreen.js
@@ -1,45 +1,59 @@
-import React from 'react'
-import { Text, View, StyleSheet, Image, Button } from 'react-native'
-import LoginForm from './components/LoginForm'
+import React from "react";
+import { Text, View, StyleSheet, Image, Button } from "react-native";
+import LoginFormRF from "./components/LoginFormRF";
+import AuthMiddleware from "./middleware/AuthMiddleware";
+import { connect } from "react-redux";
 
-export default class NewUserScreen extends React.Component {
-    static navigationOptions = {
-        title: 'Login',
-        header: null,
-    }
+class NewUserScreen extends React.Component {
+  static navigationOptions = {
+    title: "New User Screen",
+    header: null
+  };
 
-    render() {
-        return (
-            <View>
+  render() {
+    const { createUserWithEmailAndPassword } = this.props;
 
-                <Text style={styles.title}>Create Account</Text>
-                <Text>TODO: Add form to create new user account</Text>
-
-                <Button title="Already Have an Account?"
-                    onPress={() => {
-                        this.props.navigation.navigate('LoginScreen')
-                    }} />
-            </View>
-        )
-    }
-
-
+    const submit = ({ email, password }) => {
+      createUserWithEmailAndPassword(email, password);
+    };
+    return (
+      <View>
+        <Text style={styles.title}>Create Account</Text>
+        <LoginFormRF onSubmit={submit} />
+        <Button
+          title="Already Have an Account?"
+          onPress={() => {
+            this.props.navigation.navigate("LoginScreen");
+          }}
+        />
+      </View>
+    );
+  }
+}
+const mapDispatchToProps = dispatch => {
+  return {
+    createUserWithEmailAndPassword: (email, password) =>
+      AuthMiddleware.createUserWithEmailAndPassword(email, password)(dispatch)
+  };
 };
 
+export default connect(
+  null,
+  mapDispatchToProps
+)(NewUserScreen);
 const styles = StyleSheet.create({
-    image: {
-        width: 200,
-        height: 200,
-        resizeMode: 'contain',
-        marginTop: 30,
-        marginLeft: 110
-    },
-    title: {
-        fontSize: 30,
-        marginTop: 10,
-        color: 'rgba(96,100,109, 1)',
-        lineHeight: 50,
-        textAlign: 'center'
-    }
-})
-
+  image: {
+    width: 200,
+    height: 200,
+    resizeMode: "contain",
+    marginTop: 30,
+    marginLeft: 110
+  },
+  title: {
+    fontSize: 30,
+    marginTop: 10,
+    color: "rgba(96,100,109, 1)",
+    lineHeight: 50,
+    textAlign: "center"
+  }
+});
