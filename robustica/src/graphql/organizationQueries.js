ec973547425679f77dc26b91e75b92332b4a2536
Davis Mariotti <davismariotti@gmail.com>
2019-05-07 22:24:22 -0700

    Make stripe settings editable on settings page

--- robustica/src/graphql/organizationQueries.js
+++ robustica/src/graphql/organizationQueries.js
@@ -11,10 +11,39 @@ query ReadOrganization($organizationId: Long!) {
 }
 `
 
+export const organizationReadAdminQuery = gql`
+query ReadOrganizationAdmin($organizationId: Long!) {
+  organization {
+    read(id: $organizationId) {
+      id
+      name
+      stripeDetails {
+        stripePublishableKey
+        stripeSecretKey
+      }
+    }
+  }
+}
+`
+
 export const editOrganizationMutation = gql`
 mutation EditOrganization($organizationId: Long!, $organizationInput: OrganizationInput!) {
   organization {
     update(organizationId: $organizationId, organizationInput: $organizationInput) {
+      id
+      stripeDetails {
+        stripePublishableKey
+        stripeSecretKey
+      }
+    }
+  }
+}
+`
+
+export const updateStripeDetailsMutation = gql`
+mutation UpdateStripeDetails($organizationId: Long!, $stripePublishableKey: String, $stripeSecretKey: String) {
+  organization {
+    updateStripeDetails(organizationId: $organizationId, stripePublishableKey: $stripePublishableKey, stripeSecretKey: $stripeSecretKey) {
       id
     }
   }
