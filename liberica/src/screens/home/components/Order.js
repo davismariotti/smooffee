d60b8a7a6700dd037bab2510b189fc962d3efbbf
Tersa Almaw <talmaw19@my.whitworth.edu>
2019-05-17 09:16:28 -0700

    Current Order, The list of orders are separated into two types of orders, Active Order or Completed Order.

--- liberica/src/screens/home/components/Order.js
+++ liberica/src/screens/home/components/Order.js
@@ -1,6 +1,7 @@
-import React, { Component } from 'react'
-import { Button, Text, View, } from 'react-native'
-
+import React, {Component} from 'react'
+import {Button, StyleSheet, Text, View,} from 'react-native'
+import {Card} from "react-native-elements";
+import {formatCurrency} from '../../../utils/currencyUtils'
 class Order extends Component {
   handleOptionsButton() {
 
@@ -8,14 +9,43 @@ class Order extends Component {
 
   render() {
     return (
-      <View>
-        <Text>{this.props.name}</Text>
-        <Text>{this.props.price}</Text>
-        <Text>{this.props.description}</Text>
-        <Button title='options' onPress={this.handleOptionsButton.bind(this)}/>
+      <View style={{paddingBottom:10 }}>
+        <Card containerStyle={{padding: 40, width:300}}>
+          <View style={styles.rowAlign}>
+            <Text style={styles.title}>{this.props.name}</Text>
+            <Text style={styles.money}>   {formatCurrency(this.props.price)}</Text>
+          </View>
+          <Text>{this.props.description}</Text>
+          <View style={styles.rowAlign}>
+            <Text style={styles.timeAndSpace}>Location</Text>
+            <Text style={styles.timeAndSpace}>Time</Text>
+          </View>
+        </Card>
       </View>
     )
   }
 }
+
+const styles = StyleSheet.create({
+  timeAndSpace:{
+    color:'grey'
+  },
+  money:{
+    color:'red'
+  },
+  rowAlign:{
+    flexDirection:'row',
+    justifyContent:'space-between'
+  },
+  container: {
+    alignItems: 'center'
+  },
+  title: {
+    textAlign: 'center',
+    fontSize: 20,
+    fontWeight: 'bold',
+    color: 'black'
+  },
+})
 
 export default Order
--- liberica/src/screens/home/components/ScrollingOrders.js
+++ liberica/src/screens/home/components/ScrollingOrders.js
@@ -1,10 +1,12 @@
 import React, { Component } from 'react'
-import { ScrollView } from 'react-native'
+import {FlatList, View, StyleSheet, ScrollView, Text} from 'react-native'
 import { graphql } from 'react-apollo'
 
 import Order from './Order'
 import { readOrdersQuery } from '../../../graphql/userQueries'
 import LoadScreen from '../../LoadScreen'
+import Status from "../../../utils/Status";
+import {Card, ListItem} from "react-native-elements";
 
 class ScrollingOrders extends Component {
   render() {
@@ -16,21 +18,89 @@ class ScrollingOrders extends Component {
         <LoadScreen/>
       )
     }
-
+    const orders = readOrdersQueryResult.user.currentUser.orders
+    const activeOrders = orders.filter(order => (order.status === Status.ACTIVE))
     return (
       <ScrollView>
-        {readOrdersQueryResult.user.currentUser.orders.map(order => {
-          return (
-            <Order key={order.id} name={order.product.name} price={order.product.price} description={order.product.description}/>
-          )
-        })}
+        <Text style={styles.title}>Active Orders</Text>
+          <View style={styles.active}>
+            {readOrdersQueryResult.user.currentUser.orders.filter(order => (order.status === Status.ACTIVE || order.status === Status.IN_PROGRESS)).map(order => {
+              return (
+                  <Order
+                      key={order.id}
+                      name={order.product.name}
+                      price={order.product.price}
+                      description={order.product.description}
+                  />
+              )
+            })}
+
+          </View>
+
+        <Text style={styles.titleCompleted}>Completed Orders</Text>
+
+            <View style={styles.completed}>
+              {readOrdersQueryResult.user.currentUser.orders.filter(order => (order.status === Status.COMPLETED)).map(order => {
+                return (
+                  <Order
+                    key={order.id}
+                    name={order.product.name}
+                    price={order.product.price}
+                    description={order.product.description}
+                />
+                )
+              })}
+
+            </View>
+
       </ScrollView>
+
 
     )
   }
 }
+const styles = StyleSheet.create({
+  container: {
+    alignItems: 'center'
+  },
+  title: {
+    textAlign: 'center',
+    fontSize: 20,
+    fontWeight: 'bold',
+    color: 'red'
+  },
+  titleCompleted: {
+    textAlign:'center',
+    fontSize: 20,
+    fontWeight: 'bold',
+    color: 'green'
+  },
+  completed: {
+    alignItems: 'center'
+  },
+  active: {
+    alignItems: 'center'
+  }
+});
 
 export default graphql(readOrdersQuery, {
-  name: 'readOrdersQueryResult'
+  name: 'readOrdersQueryResult',
+  options: {
+    variables: {
+      organizationId: 3,
+      parameters: {
+        order: [
+          'name',
+          'asc'
+        ],
+        filter: {
+          eq: {
+            field: 'status',
+            value: Status.ACTIVE
+          }
+        }
+      }
+    }
+  }
 })
 (ScrollingOrders)
